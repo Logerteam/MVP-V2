083ca5e577f0f0672d18ead447e350f8f1724220
Loger <team@logerit.com>
2025-08-08 20:29:07 +0530

    Update Order.js

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,17 +1,41 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  customer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  rider: { type: mongoose.Schema.Types.ObjectId, ref: 'Rider', default: null },
-  pickupAddress: { type: String, required: true },
-  dropoffAddress: { type: String, required: true },
-  status: { 
-    type: String, 
-    enum: ['created', 'assigned', 'in_transit', 'delivered', 'pod_captured'], 
-    default: 'created' 
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   },
-  podImageUrl: { type: String, default: null },
-  price: { type: Number, required: true }
-}, { timestamps: true });
+  pickupLocation: {
+    type: String,
+    required: true
+  },
+  dropLocations: [{
+    address: { type: String, required: true },
+    contactName: String,
+    contactPhone: String
+  }],
+  weight: Number,
+  deliveryWindow: {
+    start: Date,
+    end: Date
+  },
+  reversePickup: {
+    type: Boolean,
+    default: false
+  },
+  rider: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Rider'
+  },
+  status: {
+    type: String,
+    enum: ['assigned', 'in_transit', 'completed', 'cancelled'],
+    default: 'assigned'
+  },
+  podImageUrl: String
+}, {
+  timestamps: true
+});
 
 module.exports = mongoose.model('Order', orderSchema);
